a51e792c03ac2e79120facd92cdc4f5399086ba1
mastakb1 <99850986+mastakb1@users.noreply.github.com>
2024-06-21 15:26:14 +0700

    Update awVisualization.py

--- awVisualization.py
+++ awVisualization.py
@@ -1,11 +1,11 @@
 import plotly.express as px
 
+def plot_scatter_plot(data, color='green'):
+    fig = px.scatter(data, x='OrderQty', y='LineTotal', title='Relasi Antara Order Quantity dan Total Penjualan', color_discrete_sequence=[color])
+    return fig
+    
 def plot_line_chart(data, color='blue'):
     fig = px.line(data, x=data.index, y='LineTotal', title='Tren Penjualan', color_discrete_sequence=[color])
-    return fig
-
-def plot_scatter_plot(data, color='blue'):
-    fig = px.scatter(data, x='OrderQty', y='LineTotal', title='Relasi Antara Order Quantity dan Total Penjualan', color_discrete_sequence=[color])
     return fig
 
 def plot_pie_chart(data):
